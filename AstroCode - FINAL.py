46c836f5475baeb080dbe6c17bf95d3436665b96
Mukesh Dharanibalan <107654613+muk03@users.noreply.github.com>
2024-02-22 17:06:54 +0000

    Polished, Commented, ready to report

--- AstroCode - FINAL.py
+++ AstroCode - FINAL.py
@@ -1,6 +1,7 @@
 #%%
+
+# Astropy, skimage (sci-kit) may need to be pip-installed
 import numpy as np 
-import astropy
 from astropy.io import fits
 import matplotlib.pyplot as plt
 from scipy.stats import skewnorm, poisson
@@ -11,6 +12,7 @@ from skimage import measure
 from scipy.stats import ks_2samp
 
 #%%
+
 #loading in data
 hdulist = fits.open("mosaic.fits")
 
@@ -19,24 +21,34 @@ pixelvalues = hdulist[0].data
 pixelvalues_cropped_horizontally = pixelvalues[:, 120:-120]
 
 # Crop vertically by slicing only rows from 110 to -110, keeping all columns
+# this to get rid of the image borders and noisy edge data in the image itself.
 pixelvalues = pixelvalues_cropped_horizontally[110:-110, :]
 
 #%%
-#plottinh histogram, mask used to remove anomolies/ clean data
+#plotting histogram, mask used to remove anomolies/ clean data
+# narrows data down to just our background/feature space, 
+# removes super bright, or super dim data
+
 mask = (3000 < pixelvalues) & (pixelvalues < 4000)
 
 # Use the mask to select pixels within the desired range
 selected_pixels = pixelvalues[mask]
 
+
+# histogram is clearly asymmetric, as expected from a poisson distribution
+# best to use a skewed gaussian, as fitting a poisson was technically difficult
 def skewed_gaussian(x, amp, a, mean, std):
     return amp * skewnorm.pdf(x, a, loc=mean, scale=std)
 
+# extract key values
 hist, bins = np.histogram(selected_pixels, bins = 1000, density=True)
 
 midpoints = (bins[1:] - bins[:-1])/2
 
+# fit values
 params, covariance = curve_fit(skewed_gaussian, bins[:-1], hist, p0 = (900000, 1.4, 3425, np.sqrt(3425)), maxfev = 100000)
 
+# plot to show results. Mean = global background value
 plt.figure(figsize=(10, 10))
 plt.bar(bins[:-1], hist, width = np.diff(bins), edgecolor='black', label = 'Data ')
 plt.plot(bins[:-1], skewed_gaussian(bins[:-1], *params), label = 'Gauss fit', color = 'red')
@@ -48,9 +60,15 @@ plt.grid()
 plt.xlim(3300,3700)
 plt.show()
 
-#the mean of this histogram should be like the background noise
+
 
 #%%
+
+# produce binary dataset that gets rid of any pixels that are below our threshold
+# threshold was chosen to be mean + 3 * std
+
+# this allows us to do further processing
+
 def masker(data, cutoff):
     max_length = max(len(sublist) for sublist in data)
     data = np.array([np.pad(sublist, (0, max_length - len(sublist)), 'constant', constant_values=0) for sublist in data])
@@ -76,11 +94,20 @@ Apass = 1800
 
 #%%
 
-# THIS IS CONFIRM GOOD SHIT
-# I want to go back to the og data without the hot stuff
+
+# I want to go back to the data without the ultra-bright/blooming/bleeding
+
+# this uses some scripts to return the local backgrounds as appropriate while 
+# removing ultra-bright star, and any other blooming/bleeding
+
+# remove objects that have an area greater than or equal to 
+# 1850px^2
+
 def skimmyging_with_change_indicator(data, cutoff, area):
 
     # Create a copy of the data to work with
+    # prevents python messing up memory and storages
+
     storage = np.copy(data)
     
     # Apply the mask to identify bright sources
@@ -92,18 +119,19 @@ def skimmyging_with_change_indicator(data, cutoff, area):
     # Initialize the change indicator array with zeros
     change_indicator = np.zeros(storage.shape, dtype=int)
     
-    # Measure properties of labeled objects
+    # skimage does a lot of the heavy lifting for is
     regions = measure.regionprops(label_image)
 
     for region in regions:
         if region.area >= area:
             for coordinates in region.coords:
-                # Set pixels of this region to 0 in the original data
                 storage[coordinates[0], coordinates[1]] = 0
-                # Mark the change in the change indicator array
                 change_indicator[coordinates[0], coordinates[1]] = 1
 
-    # Optionally, you can visualize the change_indicator array
+    # to see exactly what has been removed.
+    
+    # concurrent with what is desired
+                
     plt.imshow(change_indicator, cmap = 'gray')
     plt.show()
 
@@ -113,7 +141,12 @@ skim_data, changer = skimmyging_with_change_indicator(pixelvalues, cutoffSet, 18
 
 #%%
 
-# THIS IS ALSO GOOD
+# by using the aforementioend, where the coordinates of the large, bright
+# objects have been found, its indices are used here to remove them by
+# setting their values to zero in the actual data.
+
+# as such, background and other noise relevant later is preserved.
+
 def compare(data, indicator):
     
     for i in range(len(data)):
@@ -126,31 +159,23 @@ no_blooming_bg = compare(pixelvalues, changer)
 #plotting this isn't too nice
 
 #%%
-#trying to save to a fits file to 
-# THIS IS GOOD
+#trying to save to a fits file 
+
+# if you see this in DS9, it is very clear that the job is done well
+
 hdu = fits.PrimaryHDU(no_blooming_bg)
-
-# Create a HDUList to contain the HDU
 hdulist2 = fits.HDUList([hdu])
-
-# Define the name of the FITS file
 filename = 'no_blooming_bg.fits'
-
-# Write the HDUList to a new FITS file
 hdulist2.writeto(filename, overwrite=True)
-#this plot looks pretty good
-
-#%%
-def ideal(N, C):
-    ideal = 0.6*np.array(N) + C
-    
-    return ideal
 
 # %%
 
 # PHOTOMETRY SECTION BEGINS HERE
 
-# THIS IS GOOD
+
+# all the "extractor" modules are there to appropriately extract information
+# while preserving the shape of the matrix to avoid any confusions later
+
 def extractor(matrix, indices):
 
     # This for query-generated indices
@@ -161,7 +186,6 @@ def extractor(matrix, indices):
 
     return extracted_values
 
-# THIS IS GOOD
 def extractorNonBool(matrix, indices):
     
     # the same but for n x 2 arrays with x and y indices for each column
@@ -173,24 +197,34 @@ def extractorNonBool(matrix, indices):
     
     return extracted_values
 
-# THIS IS GOOD
 def extractorBackground(matrix, indices, setValue):
 
     # Some issues when implementing for background, where the matrix was
-    # getting wrongly updated
+    # getting wrongly updated, so have to do it this way, didn't want to risk
+    # changing to np.copy and that
+
     matOut = np.zeros_like(matrix)
     matOut += matrix
     row_indices, col_indices = indices[:, 0], indices[:, 1]
     
     # to set all the hot-pixels to the mean background radiation value 
-    # of 3409 AKA mu
+    # of 3409 AKA mu - this only helps in our galaxy analysis
+
+    # later, we will see how using a process without this is necessary
+    # for error analysis
+
     matOut[row_indices, col_indices] = setValue
     
     return matOut
 
-# THIS LOOKS GOOD
+
+# this function is important for finding the indices of the hot pixels
+# these indices are then used later for further processing
+
+# hot pixels are usually isolated, and so have no adjacent non-zero pixels
+# and so the connectivity feature on sk-image is used here to do the heavy-lifting
 def lonelyRemove(binary_array):
-    # Label connected components
+
     labeled_array, num_features = measure.label(binary_array, connectivity=2, return_num=True)
 
     # Count sizes of each connected component
@@ -208,15 +242,20 @@ def lonelyRemove(binary_array):
 
     return filtered_binary_array, non_lone_positions
 
-# IM NOT SURE ABOUT THIS FUNCTION ONLY
-# THIS MIGHT HAVE SOME PROBLEMS - DOES NOT
 
+# exists simply to establish thresholds on counts
+# data is unreliable above 50000 counts due to detector limitations
+# so any features (connectivity) with a maximum pixel value > 50000 is removed
+
+# likewise, some noise from earlier blooming removal is still present
+# putting a lower threshold to remove these here
+
+# cannot simply change mu + 3 * std as the local backgrounds are all different
+# making it impossible to do correctly back then
 
 def FinalClearance(matrix, threshold, threshold2):
 
     labeled_features = measure.label(matrix != 0)
-
-
     max_values = []
     for props in measure.regionprops(labeled_features):
         max_value = np.max(matrix[props.coords[:, 0], props.coords[:, 1]])
@@ -237,6 +276,9 @@ def FinalClearance(matrix, threshold, threshold2):
 
     return matrix, remaining_indices
 
+# combines lonelyRemove and FinalClearance
+
+# returns galaxy indices
 def combinedRemove(binaryArray, matrix, upperBound, lowerBound):
     # Apply FinalClearance on the matrix
 
@@ -250,7 +292,9 @@ def combinedRemove(binaryArray, matrix, upperBound, lowerBound):
 
     return cleaned_matrix, final_matrix, non_lone_positions
 
-# THERE IS A PROBLEM HERE PROBABLY
+
+# uses a rectangular annulus around any features (same connectivity 
+# method to identify) take median to prevent any clipping issues 
 
 def medianBack(matrix, feature_value=0, annulus_width=12):
     
@@ -278,7 +322,9 @@ def medianBack(matrix, feature_value=0, annulus_width=12):
     
     return medians
 
-# MAGNITUDE DETERMINE IS ALL GOOD
+# Median values are index matches to indices of the features.
+# easy to go through, use same library to subtract median values from 
+# corresponding features/galaxies
 def magDetermine(matrix, median_values):
     
     matrix_copy = np.copy(matrix).astype(float)
@@ -296,11 +342,13 @@ def magDetermine(matrix, median_values):
 
     return matrix_copy
 
+# count number of galaxies found after all
 def galaxyNum(matrix):
     # Label connected components of features
     labeled_features = measure.label(matrix != 0)
 
-    # Count the number of unique labels (excluding 0, which represents the background)
+    # Count the number of unique labels 
+    # (excluding 0, which represents the background)
     num_features = len(np.unique(labeled_features)) - 1
 
     return num_features
@@ -311,48 +359,64 @@ def galaxyNum(matrix):
 
 noBloomValues = no_blooming_bg
 
-
+# combines all the above
 def SequentialImageProcessor(data, backgroundBounds, starBounds, dustBounds, 
                              meanBackground, annulusWidth, filename):
+    
+    # find indices of pixels above mean + 3 * std value 
+    # - essentially just the features
     galaxyIndex = data > backgroundBounds
     booleanGalaxy = np.array(galaxyIndex).astype(int)
 
+    # find indices of pixels below mean + 3 * std value
+    # - essentially just hte background
     backgroundIndex = data <= backgroundBounds
     booleanBackground = np.array(backgroundIndex).astype(int)
 
+    # actually find the data of these indices in counts, instead of just binary
     galaxies = extractor(data, galaxyIndex)
     background = extractor(data, backgroundIndex)
 
+    # remove hotpixels etc, and remove upper and lower threshold data as required
     cleanBinaryGalaxies, cleanCountGalaxy, ObjectPositions = \
     combinedRemove(booleanGalaxy, galaxies, starBounds, dustBounds)
+    
+    # invert cleaned out (no-hot-pixel) data from the binary feature-only dataset
+    # find where this non-hotpixel and our aformentioned booleanbackground 
+    # values mismatch
 
     inverted = np.logical_not(cleanBinaryGalaxies)
     hotPixels = np.argwhere((inverted & (booleanBackground == 0)))
 
+    # use these to set hot pixels to background, producing a uniform background only
+    # dataset
     cleanBackground = extractorBackground(background, hotPixels, meanBackground)
 
+    # features/galaxies in cleanBackground are zero-valued.
+    # find these using skimage, and use a rectangular annulus to calculate median
+    # background around these galaxies
     countAdjustment = medianBack(cleanBackground, feature_value=0, annulus_width=annulusWidth)
 
+    # remove local background from the galaxy's counts to correctly
+    # find galaxy's magnitude in terms of 
     adjustedGalaxyMagnitudes = magDetermine(cleanCountGalaxy, countAdjustment)
     
     endItAll = np.copy(adjustedGalaxyMagnitudes)
 
-    countsError = np.sqrt(endItAll)
-
+    # save to see in DS9 clearly
     mags = fits.PrimaryHDU(endItAll)
-
     magList = fits.HDUList([mags])
-
     name = f'{filename}.fits'
-
     magList.writeto(name, overwrite=True)
 
+    # print number of observed galaxies
     galaxyCount = galaxyNum(endItAll)
 
     print(galaxyCount)
 
     return endItAll, galaxyCount, ObjectPositions
 
+# using equation ins cript and header data to calcualte flux appropriately
 def FluxCalibration(inputMatrix):
 
     with fits.open('mosaic.fits') as hdul:
@@ -375,6 +439,7 @@ def FluxCalibration(inputMatrix):
 
     return magCalibrated
 
+# need sum of allc ounts per feature to correctly calculate
 def countSum(image_data):
     labeled_image = measure.label(image_data != 0)
 
@@ -393,11 +458,16 @@ def countSum(image_data):
 
     return sum_of_values_per_feature
 
-
+# important fit functions
 def schechterFinal(magnitude, phi_star, m_star, alpha):
     return phi_star * np.exp(-10**(0.4 * (magnitude - m_star))) * (10**(0.4 * (magnitude - m_star)))**(alpha+1)
 
+def ideal(N, C):
+    ideal = 0.6*np.array(N) + C
+    
+    return ideal
 
+# statistical tests, plotting, curve-fitting and many more
 def FinalPropagations(inputData):
 
     # ------------------------------------------------------------
@@ -520,6 +590,7 @@ def FinalPropagations(inputData):
     return sums, mid, freq, logcdf, vals, reducedChi2lab, reducedChi2linear
 
 
+
 # %%
 
 galaxyMags, totalCount, posGalaxy = SequentialImageProcessor(no_blooming_bg, mu + 3 * std, 50000, mu + 3 * std + 30,  mu, 12, 'FinalFITS')
@@ -539,8 +610,8 @@ def SchechterPropagations(inputData):
 
     # ------------------------------------------------------------
     # extracting relevant information to see the actual number distributions
-    freq, bins = np.histogram(calibratedInfo, bins = 50)
-    freqNormalised, bins = np.histogram(calibratedInfo, bins = 50, density = True)
+    freq, bins = np.histogram(calibratedInfo, bins = 40)
+    freqNormalised, bins = np.histogram(calibratedInfo, bins = 40, density = True)
     
     mid = (bins[1:] + bins[:-1]) / 2
     bin_width = np.diff(bins)[0]
@@ -593,6 +664,11 @@ def SchechterPropagations(inputData):
 
     schecParams, schecCov = curve_fit(schechterFinal, sXdata, sYdata, [0.001, 23.2, -1.01], maxfev = 100000)
 
+    def AmpScaler(magnitude, amp):
+        return amp * schechterFinal(magnitude, *schecParams)
+
+    finalSchec, finalCov = curve_fit(AmpScaler, mid, freq, p0 = 320, maxfev = 100000)
+
     # ------------------------------------------------------------
     # Schechter Function's CDF Calculation:
     
@@ -631,19 +707,29 @@ def SchechterPropagations(inputData):
     # ----------------------------------------------------------
     # This is for all the plotting 
 
-    plt.figure(figsize=(10, 10))
+    plt.figure(figsize=(10, 10), dpi = 1200)
 
-    plt.plot(calibratedInfo, ydat, alpha=1, color='red', label='Linear Fit')
-    plt.plot(calibratedInfo, ideal(calibratedInfo, p0f), '-', alpha=0.5, color='grey', label='Ideal Equation (2)')    
+    #plt.plot(calibratedInfo, ydat, alpha=1, color='red', label='Linear Fit')
+    plt.plot(calibratedInfo, ideal(calibratedInfo, p0f), '-', alpha=0.5, color='grey', label='Lab Script Equation')    
     plt.errorbar(vals, logcdf, yerr=logcdf*np.sqrt(cdf)/cdf, xerr=x_errperc, fmt='x', color='black', capsize=5, label='Data')
-    plt.plot(vals, CDFcheck, color = 'blue', label='Schechter Fit - CDF')
+    plt.plot(vals, CDFcheck, color = 'blue', linestyle = 'dashed', label='Schechter Fit - CDF')
     plt.grid(True)
-    plt.title('Cumulative Number Distribution')
-    plt.xlabel('Magnitude')
-    plt.ylabel('log($N(m)$)')
-    plt.legend()
+    plt.title('Cumulative Number Distribution', fontsize = 15)
+    plt.xlabel('Magnitude', fontsize = 15, loc = 'right')
+    plt.ylabel('log($N(m)$)', fontsize = 15, loc = 'top')
+    plt.legend(fontsize = 15)
+    plt.ylim(0, 4.5)
+    plt.xlim(15.2, 26)
+
+    ax_small = plt.axes([0.575, 0.175, 0.30, 0.30])  # Create a smaller set of axes at the specified position within the main plot area
+    ax_small.errorbar(mid, freq, yerr=y_err1*freq, xerr=x_err1, fmt='o', color='blue', label='Frequency')
+    ax_small.plot(mid, finalSchec * schechterFinal(mid, *schecParams), color = 'green', label = 'Schechter PDF')
+    ax_small.set_title('Galaxy Number Distribution', fontsize = 15)
+    ax_small.set_xlabel('Magnitude', fontsize=15, loc = 'right')
+    ax_small.set_ylabel('Frequency', fontsize=15, loc = 'top')
 
     plt.show()
+
     return schecParams, schecCov
 
 # %%
